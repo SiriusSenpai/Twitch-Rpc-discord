ac5b0dc42ea83722cbacc03445253308f5f1be6b
Sirius <94825474+SiriusSenpai@users.noreply.github.com>
2022-05-03 11:21:00 -0700

    Add files via upload

---
+++ rpc.py
@@ -0,0 +1,50 @@
+from turtle import color
+import discord
+from discord.ext import commands
+from pystyle import Colors, Colorate
+from colored import fg
+import os 
+
+
+
+
+
+color = fg('#8c00ff')
+color2 = '\x1b[38;5;196m'
+
+os.system('cls')
+
+os.system(f'cls & mode 85,20 & title [Twitch Rpc]'),
+print(Colorate.Horizontal(Colors.blue_to_purple,
+f"""
+
+████████╗██╗    ██╗██╗████████╗ ██████╗██╗  ██╗
+╚══██╔══╝██║    ██║██║╚══██╔══╝██╔════╝██║  ██║
+   ██║   ██║ █╗ ██║██║   ██║   ██║     ███████║
+   ██║   ██║███╗██║██║   ██║   ██║     ██╔══██║
+   ██║   ╚███╔███╔╝██║   ██║   ╚██████╗██║  ██║
+   ╚═╝    ╚══╝╚══╝ ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝
+   made by Sirius#7749
+   
+ 
+"""))
+
+token = input(f"{color} <!> Enter Token> ")
+
+client = commands.Bot(command_prefix='.', self_bot=True, help_command=None)
+
+text = input(f"{color} <!> Enter Streaming Text> ")
+
+url = input(f"{color} <!> Enter Twitch Or Youtube Url> ")
+
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Streaming(name= f'{text}', url=f'{url}'))
+    print(f"{color} <!> Rpc Connecting...")
+    print(f"{color} <!> Rpc Connected To {client.user}")
+    
+
+
+client.run(token, bot = False)
---
+++ start.bat
@@ -0,0 +1,2 @@
+python -m pip install -r modules.txt
+python rpc.py 
